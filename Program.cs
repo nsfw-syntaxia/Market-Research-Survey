9df1d0e0f7a7235945334b7a629f52a5dc6f60bd
Trixie Dolera <trixie.dolera@cit.edu>
2025-02-08 19:03:12 +0800

    Update MarketResearchSurvey.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-namespace W3LabActivity_Dolera
+namespace MRS_Dolera
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace W3LabActivity_Dolera
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MarketResearchSurvey());
+            Application.Run(new MRS_Dolera());
         }
     }
 }
